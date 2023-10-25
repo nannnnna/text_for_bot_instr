25e54459f9662c26c2ec025f2b54a5f48d219b81
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 14:35:59 +0300

    рабочее

--- text.py
+++ text.py
@@ -4,11 +4,14 @@ import PyPDF2
 def convert_pdf_to_text(file_path):
     with open(file_path, "rb") as file:
         reader = PyPDF2.PdfReader(file)
-        text = ""
+        texts = []
         for page in range(len(reader.pages)):
-            text += reader.pages[page].extract_text()
-    return text
+            texts.append(reader.pages[page].extract_text())
+    return texts
 
-
-text_from_pdf = convert_pdf_to_text("inst.pdf")
-print(text_from_pdf)
+texts_from_pdf = convert_pdf_to_text("inst.pdf")
+print(texts_from_pdf)
+# for page_num, text in enumerate(texts_from_pdf, 1): вывод всего текста с допоплнительной нумерацией страниц
+#     print(f"------ Page {page_num} ------")
+#     print(text)
+#     print("-----------------------------\n")
