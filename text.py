88574b747a3a57b44fb92edbdae36eba4a42df68
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 15:02:20 +0300

    иду к пунктам меню для кнопок

--- text.py
+++ text.py
@@ -25,8 +25,13 @@ text_from_pages_2_3 = convert_pdf_to_text_range("inst.pdf", 2, 3)
 
 # Удаление последовательности точек
 cleaned_text = re.sub(r'\.{2,}', '', text_from_pages_2_3)
+# print(cleaned_text)
+# Функция для фильтрации текста по заданному шаблону
+def filter_by_pattern(text):
+    return "\n".join([line for line in text.split("\n") if re.search(r'\d', line)])
 
-print(cleaned_text)
+filtered_text = filter_by_pattern(cleaned_text)
+print(filtered_text)
 # for page_num, text in enumerate(texts_from_pdf, 1): вывод всего текста с допоплнительной нумерацией страниц
 #     print(f"------ Page {page_num} ------")
 #     print(text)
