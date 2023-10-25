eaf852f13340a182d223b5369225ca23de8b6b2f
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 15:08:29 +0300

    for two instructions

--- text.py
+++ text.py
@@ -21,17 +21,20 @@ def convert_pdf_to_text_range(file_path, start_page, end_page):
             text += reader.pages[page].extract_text()
     return text
 
-text_from_pages_2_3 = convert_pdf_to_text_range("inst.pdf", 2, 3)
-
+text_from_inst= convert_pdf_to_text_range("inst.pdf", 2, 3)
+text_from_oper= convert_pdf_to_text_range("inst.pdf", 2, 2)
 # Удаление последовательности точек
-cleaned_text = re.sub(r'\.{2,}', '', text_from_pages_2_3)
+cleaned_text_inst = re.sub(r'\.{2,}', '', text_from_inst)
+cleaned_text_oper = re.sub(r'\.{2,}', '', text_from_oper)
 # print(cleaned_text)
 # Функция для фильтрации текста по заданному шаблону
 def filter_by_pattern(text):
     return "\n".join([line for line in text.split("\n") if re.match(r'^\d+\.', line)])
 
-filtered_text = filter_by_pattern(cleaned_text)
-print(filtered_text)
+filtered_text_i = filter_by_pattern(cleaned_text_inst)
+filtered_text_o = filter_by_pattern(cleaned_text_oper)
+print(filtered_text_i)
+print(filtered_text_o)
 # for page_num, text in enumerate(texts_from_pdf, 1): вывод всего текста с допоплнительной нумерацией страниц
 #     print(f"------ Page {page_num} ------")
 #     print(text)
