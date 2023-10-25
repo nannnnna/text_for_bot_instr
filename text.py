ba2e38d8a381e1d186cd0260604fead991f936bf
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 14:41:46 +0300

    оглавление отдельно

--- text.py
+++ text.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import re
 
 # Конвертация PDF в текст
 def convert_pdf_to_text(file_path):
@@ -10,7 +11,19 @@ def convert_pdf_to_text(file_path):
     return texts
 
 texts_from_pdf = convert_pdf_to_text("inst.pdf")
-print(texts_from_pdf)
+# print(texts_from_pdf)
+
+def convert_pdf_to_text_range(file_path, start_page, end_page):
+    with open(file_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in range(start_page-1, end_page): # индексация страниц начинается с 0
+            text += reader.pages[page].extract_text()
+    return text
+
+# Получение текста со страниц 2 и 3
+text_from_pages_2_3 = convert_pdf_to_text_range("inst.pdf", 2, 3)
+print(text_from_pages_2_3)
 # for page_num, text in enumerate(texts_from_pdf, 1): вывод всего текста с допоплнительной нумерацией страниц
 #     print(f"------ Page {page_num} ------")
 #     print(text)
